2962e5ac58a39c3d267f26b44a692d471743b614
Barrod-code <rbarros@fisica.unlp.edu.ar>
2021-06-01 14:47:46 -0300

    routing changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { AuthProvider } from '../hooks/authContext';
 import Layout from './Layout';
 import ModalSwitch from './ModalSwitch';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      staleTime: 3600000,
+      cacheTime: 3600000,
+    },
+  },
+});
+
 function App() {
   return (
-    <Layout>
-      <ModalSwitch />
-    </Layout>
+    <ChakraProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <Layout>
+              <ModalSwitch />
+            </Layout>
+            <ReactQueryDevtools initialIsOpen />
+          </AuthProvider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { AuthProvider } from './hooks/authContext';
 
 import './styles/index.css';
 import App from './components/App';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      staleTime: 3600000,
-      cacheTime: 3600000,
-    },
-  },
-});
-
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <AuthProvider>
-            <App />
-            <ReactQueryDevtools initialIsOpen />
-          </AuthProvider>
-        </QueryClientProvider>
-      </BrowserRouter>
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
